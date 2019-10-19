2af1b1504526063adf6c9cf232eeb93b367b7557
Frédéric Carpentier-Lapointe <frcari@l4714-06.info.polymtl.ca>
2019-10-19 19:34:59 -0400

    results

--- TP1/code/test.sh
+++ TP1/code/test.sh
@@ -6,7 +6,7 @@ echo "algo,serie,taille,temps" > ./results.csv
 for algo in {"brute","seuil"}; do
     for testset_folder in $(ls $ex_folder); do
         for ex in $(ls ${ex_folder}/${testset_folder}); do
-            size=$(echo $ex | cut -d_ -f2)
+            size=$(echo $ex | cut -d'-' -f1)
             t=$(./tp.sh -e ./${ex_folder}/${testset_folder}/${ex} -a $algo -t)
             echo $algo,$testset_folder,$size,$t
         done
