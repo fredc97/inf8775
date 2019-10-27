4da0ede2170f87e33e96198d9797736a23cd0f84
Frédéric Carpentier-Lapointe <frcari@l4712-10.info.polymtl.ca>
2019-10-27 17:16:35 -0400

    tp2 glouton fini et debut progdyn

---
+++ TP2/main.cpp
@@ -0,0 +1,247 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <float.h>
+#include <stdlib.h>
+#include <math.h>
+#include <chrono>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+struct Roll
+{
+  int sizeRoll;
+  vector<int> sizeCuts;
+  vector<int> priceCuts;
+};
+
+struct Solution
+{
+  vector<int> sizeCuts;
+  int profit = 0;
+};
+
+struct Rentability
+{
+  int cutProfit;
+  int cutSize;
+  double ratio;
+};
+
+vector<Rentability> RemoveCut(vector<Rentability> r, int cutSize, int profit)
+{
+  r.erase(
+      std::remove_if(r.begin(), r.end(), [&](Rentability const &r2) {
+        return (r2.cutSize == cutSize && r2.cutProfit == profit);
+      }),
+      r.end());
+  return r;
+}
+
+bool cmp(const Rentability &lhs, const Rentability &rhs)
+{
+  return lhs.ratio < rhs.ratio;
+}
+
+std::ostream &operator<<(std::ostream &str, const Solution &p)
+{
+  str << "[ ";
+  for (int i = 0; i < p.sizeCuts.size(); i++)
+  {
+    str << to_string(p.sizeCuts.at(i)) + " ";
+  }
+  str << "]";
+  return str;
+}
+
+/**
+ * ----------------------------
+ * ---------------------
+ * Algorithme à programmation dynamqiue
+ * ---------------------
+ * ----------------------------
+**/
+
+Solution progDyn(Roll roll)
+{
+  Solution sol;
+  int r[roll.sizeRoll];
+  cout <<"ok";
+  vector<int> longueurCut = roll.sizeCuts;
+  vector<int> priceCut = roll.priceCuts;
+
+  for (int j = 0; j < roll.sizeRoll; j++)
+  {
+    for (int i = 0; i < roll.priceCuts.size(); i++)
+    {
+      // taille 0 
+      if (j == 0)
+      {
+        r[j] = 0;
+      }
+      else
+      {
+        
+      }
+    }
+  }
+  return sol;
+}
+
+/**
+ * ----------------------------
+ * ---------------------
+ * Algorithme glouton
+ * ---------------------
+ * ----------------------------
+**/
+
+Solution glouton(Roll roll)
+{
+  Solution sol;
+  int sum = 0;
+
+  // Rentability vector
+  vector<Rentability> rentabilities;
+  for (int i = 0; i < roll.sizeCuts.size(); i++)
+  {
+    Rentability ri;
+    ri.cutSize = roll.sizeCuts.at(i);
+    ri.cutProfit = roll.priceCuts.at(i);
+    ri.ratio = (double)ri.cutProfit / (double)ri.cutSize;
+    rentabilities.push_back(ri);
+  }
+
+  // algorithme glouton
+  while (rentabilities.size() > 0 && sum <= roll.sizeRoll)
+  {
+    Rentability r = *max_element(rentabilities.begin(), rentabilities.end(), cmp);
+    // cout << r.ratio << " : " << r.cutSize << " : " << r.cutProfit << " : " << sol.profit << " \n";
+
+    int sumTemp = sum;
+    if (sumTemp + r.cutSize <= roll.sizeRoll)
+    {
+      sum += r.cutSize;
+      sol.sizeCuts.push_back(r.cutSize);
+      sol.profit += r.cutSize * r.cutProfit;
+    }
+    else
+    {
+      rentabilities = RemoveCut(rentabilities, r.cutSize, r.cutProfit);
+    }
+  }
+
+  return sol;
+}
+
+int main(int argc, char *argv[])
+{
+  char *fn = nullptr;
+  bool show_p = false;
+  bool show_t = false;
+
+  Solution resultat;
+  double timeElapsed = 0;
+  int size = 0;
+  int elem = 0;
+
+  Roll roll;
+
+  for (int i = 1; i < argc; i++)
+  {
+
+    if (!strcmp(argv[i], "-e"))
+    {
+      i++;
+      fn = argv[i];
+      std::ifstream file;
+      file.open(fn);
+
+      if (!file)
+      {
+        cerr << "Error with file";
+      }
+
+      file >> roll.sizeRoll;
+      int totalCuts = 0;
+      while (totalCuts < roll.sizeRoll)
+      {
+        int cutTemp = 0;
+        file >> cutTemp;
+        roll.sizeCuts.push_back(cutTemp);
+        totalCuts++;
+      }
+
+      int totalPrice = 0;
+      while (totalPrice < roll.sizeRoll)
+      {
+        int priceTemp = 0;
+        file >> priceTemp;
+        roll.priceCuts.push_back(priceTemp);
+        totalPrice++;
+      }
+      /*
+      cout << "Cut size: \n";
+      for (int s = 0; s < roll.sizeCuts.size(); s++)
+      {
+        cout << roll.sizeCuts.at(s);
+        cout << "\n";
+      }
+
+      cout << "Cut price : \n";
+      for (int s = 0; s < roll.priceCuts.size(); s++)
+      {
+        cout << roll.priceCuts.at(s);
+        cout << "\n";
+      }
+*/
+    }
+    else if (!strcmp(argv[i], "-p"))
+    {
+
+      show_p = true;
+    }
+    else if (!strcmp(argv[i], "-t"))
+    {
+
+      show_t = true;
+    }
+    else if (!strcmp(argv[i], "-a"))
+    {
+      i++;
+      if (!strcmp(argv[i], "glouton"))
+      {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        resultat = glouton(roll);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        timeElapsed = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+      }
+      else if (!strcmp(argv[i], "dynamique"))
+      {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        resultat = progDyn(roll);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        timeElapsed = (double)std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+      }
+      else
+      {
+        cout << "Erreur: Algo inconnu" << endl;
+        return -1;
+      }
+    }
+  }
+
+  if (show_p)
+  {
+    cout << resultat.profit << " with : " << resultat << endl;
+  }
+  if (show_t)
+  {
+    cout << "Time elapsed : " << (double)timeElapsed << endl;
+  }
+
+  return 0;
+}
---
+++ TP2/tp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ALGO="glouton"
+OPTIONS=""
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -a)
+    ALGO="$2"
+    shift
+    ;;
+    -e)
+    EX_PATH="$2"
+    shift
+    ;;
+    -p|-t)
+    OPTIONS="${OPTIONS}${1} "
+    ;;
+    *)
+        echo "Argument inconnu: ${1}"
+        exit
+    ;;
+esac
+shift
+done
+
+
+./main -e $EX_PATH -a $ALGO $OPTIONS
